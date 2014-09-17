0122081f51998dd59f279727d4197356ba28b697
Edward Fernando <edward.fer@gmail.com>
2014-09-17 19:43:42 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vivace::Application.config.secret_token = 'dd7e032076ee791ef035b6b019af6ffe1ec69275d8124a39130d8c22f1c03527ed0d88e0b3bef0afacd58849faea235e20445d706a44e34412157c29e1a94a15'
